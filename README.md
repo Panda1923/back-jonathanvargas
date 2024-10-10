6d48a81a29708be452179cb21c1a96dbba7dd11c
Jonathan Vargas Hurtado <you@example.com>
2024-10-10 08:11:36 -0500

    primer servidor funcionando

---
+++ README.md
@@ -0,0 +1,6 @@
+debe ejecutar 
+npm install express
+crear .gitignor
+debe intalar npm install --save-dev nodemon
+devemos instalara npm install dotenv
+tener en cuenta si no funciona npm install --save-dev nodemon
---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import "dotenv/config.js"
+const server = express()
+const PORT = 8080
+const ready = ()=> console.log("server ready in port :"+PORT);
+server.get('/',(request,response) =>{
+    response.send('Hola mundo en Express')
+})
+server.get('/saludo',(request,response) =>{
+    response.send('oi en Express')
+})
+server.listen(PORT,ready)
+
+
+
