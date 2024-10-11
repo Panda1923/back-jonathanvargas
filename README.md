e9f2c781639b86bac39f9ca9c78ff434146cba38
Jonathan Vargas Hurtado <you@example.com>
2024-10-10 22:47:40 -0500

    back001

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ crear .gitignor
 debe intalar npm install --save-dev nodemon
 devemos instalara npm install dotenv
 tener en cuenta si no funciona npm install --save-dev nodemon
+installar npm install mongoose
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import "dotenv/config.js"
+import "./config/database.js"
+
 const server = express()
 const PORT = 8080
 const ready = ()=> console.log("server ready in port :"+PORT);
